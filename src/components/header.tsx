b83b540521b947c27d4fb1b7910b11a353c6e9da
Sythaz <syafiqaldiansyah@gmail.com>
2026-01-04 14:40:09 +0700

    feat: add logo images and enhance header component with theme toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router";
+import { useTheme } from "./theme-provider";
+import { Moon, Sun } from "lucide-react";
+
 const Header = () => {
-  return <header>Header</header>;
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === "dark";
+
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background backdrop-blur py-2 supports-backdrop-filter:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={"/"}>
+          <img
+            src={isDark ? "/logo.png" : "/logo2.png"}
+            alt="Klimate Logo"
+            className="h-14"
+          />
+        </Link>
+
+        <div>
+          {/* Search */}
+          <div onClick={() => setTheme(isDark ? "light" : "dark")}>
+            {isDark ? (
+              <Sun className="h-6 w-6 cursor-pointer text-yellow-500" />
+            ) : (
+              <Moon className="h-6 w-6 cursor-pointer text-blue-500" />
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
