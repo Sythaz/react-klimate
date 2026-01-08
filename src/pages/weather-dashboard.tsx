514290d11f8509ec2f2fda32e1d655cbd7297ac1
Sythaz <syafiqaldiansyah@gmail.com>
2026-01-08 17:14:02 +0700

    feat: update favicon path and implement loading skeleton in weather dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,17 +1,73 @@
-import { RefreshCw } from "lucide-react";
+import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
 import { Button } from "../components/ui/button";
 import { useGeolocation } from "../hooks/use-geolocation";
+import WeatherSkeleton from "../components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "../components/ui/alert";
 
 const WeatherDashboard = () => {
-  const { getLocation, coordinates, error, isLoading } = useGeolocation();
+  const {
+    getLocation,
+    coordinates,
+    error: locationError,
+    isLoading: locationLoading,
+  } = useGeolocation();
 
   console.log("WeatherDashboard render");
   console.log({ coordinates });
 
   const handleRefresh = () => {
-    console.log("handleRefresh");
-    getLocation();
+    if (locationError) {
+      // Minta permission lagi jika belum diberikan
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then((result) => {
+          if (result.state === "prompt" || result.state === "denied") {
+            getLocation();
+          }
+        })
+        .catch(() => {
+          getLocation();
+        });
+    } else {
+      getLocation();
+    }
   };
+
+  if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4 mr-2" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button variant="outline" onClick={handleRefresh} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!coordinates) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4 mr-2" />
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please enable location access to see your local weather</p>
+          <Button variant="outline" onClick={handleRefresh} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
 
   return (
     <div>
@@ -21,7 +77,7 @@ const WeatherDashboard = () => {
           variant={"outline"}
           size={"icon"}
           onClick={handleRefresh}
-          disabled={isLoading}
+          disabled={locationLoading}
         >
           <RefreshCw className="h-4 w-4" />
         </Button>
